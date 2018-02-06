b3aac05d57d440a657b4dc250241c8747abbd652
Daniel <danielbrogers594@gmail.com>
2018-02-06 14:05:19 -0500

    first commit

---
+++ Class1Prep3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Class1Prep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many miles have you driven? ");
+            string miles = Console.ReadLine();
+            float mi = Convert.ToInt32(miles);
+
+            Console.Write("How many gallons of gas have you consumed? ");
+            string gallons = Console.ReadLine();
+            float gal = Convert.ToInt32(gallons);
+
+            float mpg = mi / gal;
+            Console.WriteLine("Your fuel efficiency is " + mpg + " mpg");
+
+            Console.ReadLine();
+        }
+    }
+}
